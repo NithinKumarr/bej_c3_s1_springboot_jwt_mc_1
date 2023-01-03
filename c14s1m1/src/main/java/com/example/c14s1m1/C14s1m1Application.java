412bb6734985e058f14761cc3d981bc0efdcfdf8
girijaugge123 <girijaugge123@gmail.com>
2023-01-03 22:04:10 +0530

    solution added

---
+++ c14s1m1/src/main/java/com/example/c14s1m1/C14s1m1Application.java
@@ -0,0 +1,13 @@
+package com.example.c14s1m1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C14s1m1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C14s1m1Application.class, args);
+	}
+
+}
---
+++ c14s1m1/src/test/java/com/example/c14s1m1/C14s1m1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.c14s1m1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C14s1m1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
